23c328e729d87913296d4fd0cf8b51c8145dde03
oricel <orieta.celiku@gmail.com>
2017-11-15 09:23:20 -0500

    0.1.0

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='awalker',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='A Random Walker',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='OC',
+    author_email='',
+    url='https://github.com/oricel/giro',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
