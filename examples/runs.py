6ce9ebb91fbebe3853ed172f82e032fc0e278fd1
oricel <orieta.celiku@gmail.com>
2017-12-12 09:38:24 -0500

    Added runs

--- examples/runs.py
+++ examples/runs.py
@@ -16,33 +16,41 @@ import random
 import networkx as nx
 from networkx.utils import powerlaw_sequence
 
+import collections
+
 from awalker import randomcell
 
+from sys import argv
 
 if __name__ == '__main__':
-    Nlist = [500,1000,1200,1500]
-    max_iter = 2500
-    ystarlist = [0,5,10]
-    deltatlist = [1,2,3,4,4.5,5,5.5,6,7,9,11,13]
-
-    convergence = {}
-    n_runs = 2
+    Nlist = [500]
+    max_iter = 1500
+    ystarlist = [0,10]
+    deltatlist = [1,3,4,4.5,5,5.5,6,10,15]
+    
+    seed = int(argv[1])
+  
+    np.random.seed(seed=seed)
+    convergence = []
+    n_runs = 500
     for i in range(0,n_runs):
         try:
             for j in range(0,len(Nlist)):
                 for d in range(0,len(deltatlist)):
                     for y in range(0, len(ystarlist)):
                         cell = randomcell.Cell(Nlist[j])    
-                        print "run " + str(i) + " with " + str(Nlist[j]) + " nodes, " + str(max_iter) + " max iterations, " + str(ystarlist[y]) + " ystar, " + str(deltatlist[d]) + " deltat"
+                        print "run=" + str(i) + " with " + "nodes=" + str(Nlist[j]) + ", max iter=" + str(max_iter) + ", ystar=" +  str(ystarlist[y]) + ", deltat=" + str(deltatlist[d])
                         walk = randomcell.Walk(cell, max_iter=max_iter,
                                                ystar = ystarlist[y],
                                                deltat = deltatlist[d],
                                                save = False)
                         c = walk.converging
-                        convergence['run','N','max_iter','ystar','deltat'] = i, Nlist[j], max_iter, ystarlist[y], deltatlist[d]
+                        #c = 1
+                        #('run','N','max_iter','ystar','deltat') 
+                        convergence.append((i, Nlist[j], max_iter, ystarlist[y], deltatlist[d],c))
                         print "converged "+str(c)
         except Exception:
             continue
+        pickle.dump(convergence, open("examples/results/results_"+str(i)+ "_seed_"+str(seed)+"_" +str(datetime.datetime.now().date()) + ".pickle","w"))
     print convergence
 
-    pickle.dump(convergence, open("examples/results"+str(datetime.datetime.now().date()) + ".pickle","w"))
